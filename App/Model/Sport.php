34e3475991a921c32398640f56d54d15116bd05e
Ryww0 <laurent.chaitas@gmail.com>
2022-09-13 11:19:34 +0200

    Class Sport

---
+++ App/Model/Sport.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\model;
+
+class Sport
+{
+    private string $id;
+    private string $name;
+
+    public function __construct($name)
+    {
+        $this->id = uniqid();
+        $this->name = $name;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+}
